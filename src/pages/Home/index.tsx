6ab9d7717ba8abf8ea438e3e84a7d88a4489012d
Manoel Salgado <manoelsalgadoneto@gmail.com>
2025-02-06 14:06:54 -0300

    [WAPP] feat: created home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import './index.css'
+import globeIcon from '../../assets/icons/whiteGlobe.svg'
+
+interface CityListProps {
+  cities: string[];
+}
+
+const CityList: React.FC<CityListProps> = ({ cities }) => {
+  return (
+    <ul>
+      {cities.map((city, index) => (
+        <li key={index}>
+          <a href="#">{city}</a>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const Home: React.FC = () => {
+  const cities = ["Madri", "Fairbanks", "London", "Recife", "Vancouver", "Yakutsk"]
+
+  return (
+    <div className='container'>
+      <h1>Weather</h1>
+      <h2>Select a City</h2>
+      <div>
+        <img src={globeIcon} className="logo" alt="Globe logo"/>
+      </div>
+      <CityList cities={cities} />
+    </div>
+  )
+}
+
+export default Home
