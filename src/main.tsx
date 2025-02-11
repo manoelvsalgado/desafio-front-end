492faeec2f575ace806a9cba6f9f47fcbd68fa06
Manoel Salgado <manoelsalgadoneto@gmail.com>
2025-02-11 14:53:20 -0300

    [WAPP] feat: added WeatherProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import React from 'react'
+import { WeatherProvider } from './context/weatherContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+      <WeatherProvider>
+        <App />
+      </WeatherProvider>
+  </React.StrictMode>
 )
