39de65eea2ab76e9d0626f2aee7f41b557fe04d7
Manoel Salgado <manoelsalgadoneto@gmail.com>
2025-02-13 11:37:57 -0300

    [WAPP] feat: refactored pages' style

--- src/pages/City/index.tsx
+++ src/pages/City/index.tsx
@@ -1,23 +1,44 @@
-import { useNavigate } from 'react-router-dom';
-import WeatherInfo from '../../components/weatherInfoCard';
-import ShiftCard from '../../components/hourlyWeatherCard';
-import CurrentTemperature from '../../components/currentWeatherCard';
+import { Box, VStack, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import WeatherInfo from "../../components/weatherInfoCard";
+import ShiftCard from "../../components/hourlyWeatherCard";
+import CurrentTemperature from "../../components/currentWeatherCard";
 
-  const City: React.FC = () => {
-    const shifts = ["Dawn", "Morning", "Afternoon", "Night"];
-    const weatherInfo = ["Wind Speed", "Sunrise", "Sunset", "Humidity"];
-    const navigate = useNavigate();
+const City: React.FC = () => {
+  const shifts = ["Dawn", "Morning", "Afternoon", "Night"];
+  const weatherInfo = ["Wind Speed", "Sunrise", "Sunset", "Humidity"];
+  const navigate = useNavigate();
 
-    return (
-      <div className="container">
+  return (
+    <Box 
+      minH="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      bg="gray.900"
+      color="white"
+      p={6}
+    >
+      <VStack padding={6} align="center" w="full" maxW="500px">
         <CurrentTemperature />
         <ShiftCard shifts={shifts} />
         <WeatherInfo weatherInfo={weatherInfo} />
-        <button onClick={() => navigate(-1)}>
-          <p>Go back</p>
-        </button>
-      </div>
-    );
-  };
+
+        <Button 
+          onClick={() => navigate(-1)}
+          bg="blue.500"
+          color="white"
+          _hover={{ bg: "blue.600" }}
+          size="lg"
+          mt={4}
+        >
+          Go back
+        </Button>
+      </VStack>
+    </Box>
+  );
+};
 
 export default City;
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import globeIcon from '../../assets/icons/whiteGlobe.svg'
+import { Box, VStack, Heading, Image, Link, ListItem, Stack } from "@chakra-ui/react";
+import globeIcon from "../../assets/icons/whiteGlobe.svg";
 
 interface CityListProps {
   cities: string[];
@@ -6,29 +7,47 @@ interface CityListProps {
 
 const CityList: React.FC<CityListProps> = ({ cities }) => {
   return (
-    <ul>
+    <Stack padding={3} w="full" textAlign="center">
       {cities.map((city, index) => (
-        <li key={index}>
-          <a href={`/city/${city}`}>{city}</a>
-        </li>
+        <ListItem
+          key={index}
+          bg="gray.700"
+          p={3}
+          borderRadius="md"
+          transition="0.2s"
+          _hover={{ bg: "gray.600", transform: "scale(1.05)" }}
+        >
+          <Link href={`/city/${city}`} color="white" fontSize="lg" fontWeight="bold">
+            {city}
+          </Link>
+        </ListItem>
       ))}
-    </ul>
-  )
-}
+    </Stack>
+  );
+};
 
 const Home: React.FC = () => {
-  const cities = ["Dallol", "Fairbanks", "London", "Recife", "Vancouver", "Yakutsk"]
+  const cities = ["Dallol", "Fairbanks", "London", "Recife", "Vancouver", "Yakutsk"];
 
   return (
-    <div className='container'>
-      <h1>Weather</h1>
-      <h2>Select a City</h2>
-      <div>
-        <img src={globeIcon} className="logo" alt="Globe logo"/>
-      </div>
-      <CityList cities={cities} />
-    </div>
-  )
-}
+    <Box 
+      minH="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      bg="gray.900"
+      color="white"
+      p={6}
+    >
+      <VStack padding={4} align="center">
+        <Heading size="2xl">Weather</Heading>
+        <Heading size="lg" color="gray.400">Select a City</Heading>
+        <Image src={globeIcon} boxSize="100px" alt="Globe logo" />
+        <CityList cities={cities} />
+      </VStack>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
