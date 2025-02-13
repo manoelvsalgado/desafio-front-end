4ef0454a7a0358c57e8eab42db360193fb6a29e1
Manoel Salgado <manoelsalgadoneto@gmail.com>
2025-02-12 23:07:36 -0300

    [WAPP] feat: chages in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import React from 'react'
-import { WeatherProvider } from './context/weatherContext.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <WeatherProvider>
-        <App />
-      </WeatherProvider>
+    <App />
   </React.StrictMode>
 )
